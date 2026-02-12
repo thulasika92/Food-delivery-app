68756da0bf8e5fc00168ddf85992ae67d73b5867
thulasika92 <thulasikamoorthy9@gmail.com>
2026-02-12 14:09:32 +0530

    feat: add category toggle and active state styling to ExploreMenu

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
   return (
-    <div>ExploreMenu</div>
+    <div className='explore-menu ' id='explore-menu'>
+        <h1>Explore our menu</h1>
+        <p className='explore-menu-text'>Explore our diverse menu filled with freshly prepared dishes and flavors you love. From quick bites to full meals, find the perfect option to satisfy your cravings and order with ease.</p>
+        <div className="explore-menu-list">
+            {menu_list.map((item,index)=>{
+                return (
+                    <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+                        <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+                        <p>{item.menu_name}</p>
+
+                    </div>
+                )
+            })}
+        </div>
+        <hr/>
+
+    </div>
   )
 }
 
