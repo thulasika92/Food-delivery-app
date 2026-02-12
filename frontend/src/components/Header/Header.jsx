62508d369b7599044150e21a6473df7a320574da
thulasika92 <thulasikamoorthy9@gmail.com>
2026-02-12 12:26:55 +0530

    feat: add header section and setup routing

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2><h2>Hungry? We’ve Got You Covered!</h2></h2>
+            <p>Fresh and delicious food delivered to your doorstep. Explore your favorite meals and order easily in just a few clicks.</p>
+            <button>View Menu</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
